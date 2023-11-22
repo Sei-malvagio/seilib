74ddcfd5d6a601fe7b4f584dcfc979543dcff400
Sei malvagio <145441542+Sei-malvagio@users.noreply.github.com>
2023-11-22 18:09:37 +0700

    pakai ini saja

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    if(pertambahan("1", "1") == 2) {
+        printf("Aww, cinta sekaleh dengan mu.\n");
+    } else {
+        printf("Yahh... cintaku bukan untukmu nak.\n");
+    }
+}
